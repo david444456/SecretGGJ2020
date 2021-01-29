cb08977af7668bc19669f483491043bf27b2c225
david444456 <david2toranzo2@gmail.com>
2021-01-29 15:39:32 -0300

    GGJ2021 - Base Ship

---
+++ SecretGGJ2021/Assets/Script/PlayerMovementShip.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof( Rigidbody2D))]
+public class PlayerMovementShip : MonoBehaviour
+{
+    [SerializeField] float velocityForwardShip = 5;
+    [SerializeField] float rotateVelocityShip = 0.5f;
+
+    Rigidbody2D rb2D;
+    Vector2 force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move
+        if (Input.GetKey(KeyCode.W)) {
+
+            force = transform.up * velocityForwardShip * Time.deltaTime;
+            rb2D.angularVelocity = 0;
+
+            rb2D.velocity = force;
+
+
+        } else if (Input.GetKey(KeyCode.S)) {
+            Vector2 forceNegative = transform.up;
+            rb2D.AddForce(-forceNegative);
+        }
+
+        //rotate
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, 0, rotateVelocityShip));
+        } else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, 0, -rotateVelocityShip));
+        }
+
+        //stop
+        if (rb2D.velocity.x > 0)
+        {
+            rb2D.velocity += new Vector2(-Time.deltaTime, 0);
+        }
+        else if (rb2D.velocity.x < 0)
+        {
+            rb2D.velocity += new Vector2(Time.deltaTime, 0);
+        }
+
+        if (rb2D.velocity.y > 0)
+        {
+            rb2D.velocity += new Vector2(0,-Time.deltaTime);
+        } else if (rb2D.velocity.y < 0)
+        {
+            rb2D.velocity += new Vector2(0, Time.deltaTime);
+        }
+
+
+        print(rb2D.velocity);
+
+    }
+}
