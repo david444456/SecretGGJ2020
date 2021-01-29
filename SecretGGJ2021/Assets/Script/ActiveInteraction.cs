09d0dbed7e2465b08cfd1763e1f93ef806803837
david444456 <david2toranzo2@gmail.com>
2021-01-29 01:31:47 -0300

    Creación del proyecto.

---
+++ SecretGGJ2021/Assets/Script/ActiveInteraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveInteraction : MonoBehaviour
+{
+    [SerializeField] int indexPoster = 0;
+    [SerializeField] GameObject m_gameObject_Button_Activated;
+    [SerializeField] bool activePoster = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (activePoster) return;
+        if (other.tag == "Player") {
+            m_gameObject_Button_Activated.SetActive(true);
+            m_gameObject_Button_Activated.GetComponent<ActiveUIPoster>().indexPosterActual = indexPoster;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            m_gameObject_Button_Activated.SetActive(false);
+        }
+    }
+}
