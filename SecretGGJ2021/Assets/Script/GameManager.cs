0e54157203d74c5b2dff8e411f6dcf96e3d16fa0
david444456 <david2toranzo2@gmail.com>
2021-01-29 23:59:35 -0300

    Agarrar coins

---
+++ SecretGGJ2021/Assets/Script/GameManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ship
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager gameManager;
+
+        [SerializeField] int minimumValueCoins;
+        [SerializeField] int coinsToWin = 20000;
+
+        [Header("UI")]
+        [SerializeField] Text textCoin;
+
+
+        private int actualCoins = 0;
+
+        void Start()
+        {
+            gameManager = this;
+        }
+
+        public int GetMinimunValueOfRandomCoin() {
+            return minimumValueCoins;
+        }
+
+        public void AugmentValueCoins(int valueCoins) {
+            actualCoins += valueCoins;
+            textCoin.text = actualCoins.ToString();
+
+            //win
+            if (coinsToWin < actualCoins) {
+                print("Ganaste crack sos un orgullo");
+            }
+        }
+    }
+}
