66930dc28258f9e993b7d4217359a6803ca6dff3
david444456 <david2toranzo2@gmail.com>
2021-01-30 10:26:10 -0300

    Cambios script de destruir y seguir jugador

--- SecretGGJ2021/Assets/Script/DestroyShip.cs
+++ SecretGGJ2021/Assets/Script/DestroyShip.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class DestroyShip : MonoBehaviour
 {
+
+    [SerializeField] ParticleSystem particleSystemDead;
+
     private void OnCollisionEnter2D(Collision2D other)
     {
-        Destroy(gameObject);
+        if (other.gameObject.tag == "Enemy" || other.gameObject.tag == "Build")
+        {
+            particleSystemDead.Play();
+            Destroy(gameObject, 0.3f);
+        }
     }
 }
