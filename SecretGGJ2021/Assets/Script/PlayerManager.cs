eb0d0cb32de2be2ed3e56af596483dfd4a7ae936
Jonathan Tagliavini <jonathantagliavini@gmail.com>
2021-01-31 15:01:50 -0300

    Creditos agregados, fijense si falra alguien en el Credits.csv

--- SecretGGJ2021/Assets/Script/PlayerManager.cs
+++ SecretGGJ2021/Assets/Script/PlayerManager.cs
@@ -6,11 +6,23 @@ public class PlayerManager : MonoBehaviour
 {
     int points;
 
+    //[SerializeField]
+    //private UnityEngine.Object explosionRef;
+
+    public GameObject explosion;
+
+    void Start()
+    {
+        //explosionRef = Resources.Load("Explosion");
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Bomb"))
         {
+            GameObject expl = Instantiate(explosion, transform.position, Quaternion.identity) as GameObject;
             Destroy(gameObject);
+            Destroy(expl, 3);
         }
         else if (collision.CompareTag("Island"))
         {
