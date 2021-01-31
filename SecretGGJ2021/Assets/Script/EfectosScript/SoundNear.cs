89084144d49a91ea2b92246b3b0d935961f7662f
david444456 <david2toranzo2@gmail.com>
2021-01-31 13:14:07 -0300

    Arreglo de cosas, menu y demas

---
+++ SecretGGJ2021/Assets/Script/EfectosScript/SoundNear.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundNear : MonoBehaviour
+{
+    public AudioSource RuidoFuego;
+
+    [SerializeField] public GameObject Fuego;
+    [HideInInspector] GameObject Barco;
+
+    float DistanciaXY = 0;
+    float VariableDeSonido = 0;
+
+    public float EmpiezaElFuego = 15;
+
+    private void Start()
+    {
+       Barco = FindObjectOfType<PlayerMovementShip>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DistanciaXY = Vector3.Distance(Fuego.transform.position, Barco.transform.position);
+        VariableDeSonido = (DistanciaXY / EmpiezaElFuego);
+
+        if (DistanciaXY < EmpiezaElFuego)
+        {
+            RuidoFuego.volume = 1 - VariableDeSonido;
+        }
+        else
+        {
+            RuidoFuego.volume = 0;
+        }
+    }
+}
